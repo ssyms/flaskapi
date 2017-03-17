e73f047e055445176544a646e558e33cec481f98
SaradaSymonds <symonds.s@husky.neu.edu>
2017-03-17 16:24:30 +0000

    updated endpoint

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-# edit the URI below to add your RDS password and your AWS URL
-# The other elements are the same as used in the tutorial
-# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
-
-# Uncomment the line below if you want to work with a local DB
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ciscoapi:ciscoapi@ciscoapi.cllodida4od4.us-west-2.rds.amazonaws.com:3306/ciscodb'
 
 SQLALCHEMY_POOL_RECYCLE = 3600
 
